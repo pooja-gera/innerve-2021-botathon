94621cc99be4fedcdcdbbc0332e61c7e0105f962
pooja-gera <55778515+pooja-gera@users.noreply.github.com>
2021-10-18 16:55:57 +0530

    Bot finished

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 // bot prefix !1
 // role-name v1
 // role-id 899119339125481472
+// server or guild id 897324835200438273
+// channel-id 899110391643250728
 
+// Importing prefix and token from config.json
 const { prefix, token } = require("./config.json");
-
+// Adding channel ID of innerve botathon verification channel
+const verificationChannelID = "899110391643250728";
+//Adding discord.js file
 const Discord = require("discord.js");
-
 // Create an instance of a Discord client
 const client = new Discord.Client({
-    presence: {
-        status: "online",
-        activity: {
-            name: `${prefix} help for command list`,
-        }
-    }
+  presence: {
+    status: "online",
+    activity: {
+      name: `${prefix} help for command list`,
+    },
+  },
 });
 
+// Checking if the bot is connected
 client.on("ready", () => {
   console.log("I am ready!");
 });
-
-const command = require("./command");
 
 // Create an event listener for messages
 client.on("message", (message) => {
   //Doing away with the problem of the bot reading its own messages
   if (message.author.bot) return;
-
   console.log(message.content);
 
   const userId = message.author.id;
@@ -36,26 +38,57 @@ client.on("message", (message) => {
 
     Hello <@${userId}>! These are the commands I can offer you:
 
-**!1 Admission Number** - To get yourself verified and get a role assigned to yourself. 
+**!1 Admission Number** - To get yourself verified and get a role assigned to yourself. Head over to the <#${verificationChannelID}> channel and type in your admission number.
 **!1 help** - To get a list of commands.
   
 Thank-you Pooja Gera for giving me life. 
     `);
-  } else if (message.content.startsWith(`${prefix}`)) {
-    // If the message is verified
+  }
 
-    if (verify(message.content.slice(prefix.length))) {
+  //Ensuring that anything except help runs in only in the verification channel
+  else if (
+    message.content.startsWith(`${prefix}`) &&
+    message.channel.id === verificationChannelID
+  ) {
+    // If the message is verified
+    const { guild } = message;
+    const member = guild.members.cache.get(userId);
+
+    if (alreadyVerified(member, message)) {
+      message.channel.send(`You have already been verified <@${userId}>.`);
+    }
+
+    if (
+      !alreadyVerified(member, message) &&
+      verify(message.content.slice(prefix.length))
+    ) {
+      const { guild } = message;
+      const role = guild.roles.cache.find((role) => role.name === "v1");
+      const member = guild.members.cache.get(userId);
+
+      if (role) {
+        member.roles.add(role);
+      }
+
       // Send congratulations to the same channel
-      message.channel.send(`Congratulations <@${userId}>! You have been verified.`);
+      message.channel.send(
+        `Congratulations <@${userId}>! You have been verified.`
+      );
 
       //Reaction to the message
       message.react("✅").then(console.log).catch(console.error);
-    } else {
-      message.channel.send("Sorry! You have not been verified.");
-
+    } else if (!alreadyVerified(member, message)) {
+      message.channel.send("Sorry! You can not been verified.");
       //Reaction to the message
       message.react("❌").then(console.log).catch(console.error);
     }
+  } else if (
+    message.content.startsWith(`${prefix}`) &&
+    !(message.channel.id === verificationChannelID)
+  ) {
+    message.channel.send(
+      `Head over to <#${verificationChannelID}> to get yourself verified. The command will not work here.`
+    );
   }
 });
 
@@ -63,9 +96,6 @@ client.login(token);
 
 //verification function
 function verify(inputStr) {
-
-  alreadyVerified(inputStr);
-
   if (inputStr.length < 13 || inputStr.length > 16) {
     console.log("Not verified 1");
     return false;
@@ -149,15 +179,52 @@ function verify(inputStr) {
 
   //checking year
   var year = parseInt(checkArray[3]);
-  if (year > 2023 || year < 2021) {
-    console.log("Not verified 5");
-    return false;
+  var currentYear = 2021;
+
+  //range is currentYear + 1 and currentYear + jitna course h
+
+  //UG is a 4 year course
+  if (course === "UG") {
+    if (year <= currentYear + 4 && year >= currentYear + 1) {
+      console.log("Verified");
+      return true;
+    } else {
+      console.log("Not verified 5.1");
+      return false;
+    }
   }
 
-  console.log("verified!");
-  return true;
+  //PG is a 2 year course
+  else if (course === "PG") {
+    if (year <= currentYear + 2 && year >= currentYear + 1) {
+      console.log("Verified");
+      return true;
+    } else {
+      console.log("Not verified 5.1");
+      return false;
+    }
+  }
+
+  //D is a 2 year course
+  else if (course === "D") {
+    if (year <= currentYear + 2 && year >= currentYear + 1) {
+      console.log("Verified");
+      return true;
+    } else {
+      console.log("Not verified 5.1");
+      return false;
+    }
+  }
 }
 
-function alreadyVerified(admissionNumber) {
+const roleId = "899119339125481472";
+const roleName = "v1";
 
+//this will basically check if the user already has a role or not, if the user does then they've already been verified
+function alreadyVerified(member, message) {
+  if (message.member.roles.cache.has(roleId)) {
+    return true;
+  } else {
+    return false;
+  }
 }
